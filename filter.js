fa51fc73b3d2b5f54e3ef23f3e4062185a1c0b5c
Saurabh <dhakitesaurabh@gmail.com>
2024-02-18 21:39:02 +0530

    Some changes in filter.js

--- filter.js
+++ filter.js
@@ -1,7 +1,7 @@
 let expenses = [
     {amount: 2000, category: "Grocery"},
     {amount: 4000, category: "Entertainment"},
-    {amount: 200, category: "Bills"}
+    {amount: 200, category: "My Bills"}
 ]
 
 let GroceryExpense = expenses.filter((obj)=> obj.category === "Grocery")
